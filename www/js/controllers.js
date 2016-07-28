88437ce735616d346dd9358168b9a73de0699ceb
f1lander16 <daniel@leitoncr.com>
2016-07-28 01:20:50 -0600

    Splash Screen

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('starter.controllers', [])
+
+.controller('InitCtrl', function($scope) {
+
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('PaquetesCtrl', function($scope, Paquetes) {
+  $scope.paquetes = Paquetes.all();
+  $scope.remove = function(paquete) {
+    Paquetes.remove(paquete);
+  };
+})
+
+.controller('PaqueteDetailCtrl', function($scope, $stateParams, Paquetes) {
+  $scope.paquete = Paquetes.get($stateParams.paqueteId);
+});

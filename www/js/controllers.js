fb5ce118dc1a987e07a41edb8eb52a6bf47fbe85
f1lander <=>
2016-07-29 23:30:27 -0600

    fixing paquetes view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,9 +11,23 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PaquetesCtrl', function($scope, Paquetes) {
-  $scope.paquetes = Paquetes.all();
-  $scope.remove = function(paquete) {
-    Paquetes.remove(paquete);
+  $scope.items= [ [{nPaquete:1,desc:"Xbox"},{nPaquete:2,desc:"PS4"},{nPaquete:3,desc:"Wii"}]
+   ,[{nPaquete:1,desc:"Aifone"},{nPaquete:2,desc:"GBA"},{nPaquete:3,desc:"Wii u"}]
+  ,[{nPaquete:1,desc:"S7"},{nPaquete:2,desc:"huawei"},{nPaquete:3,desc:"3ds"}] ];
+  
+   $scope.activeItems = $scope.items[0];
+  
+  $scope.changeTab = function (n) {
+      $scope.activeItems = $scope.items[n];
+      $scope.$appy();
+  };
+  
+  $scope.doRefresh = function() {
+    
+   // $scope.items.push($scope.items[{nPaquete:$scope.items.length + 2,desc:"Xbox"+$scope.items.length + 2}]);
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+  
   };
 })
 
